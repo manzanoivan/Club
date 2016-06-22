61962fc35ffe580b9fc10a6de83d3960c1ac812f
manzanoivan <ivan_ajedrez@hotmail.com>
2016-06-21 19:42:02 -0500

    Grafos

---
+++ Grafo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Nodos indexados 0 -> N-1
+
+struct Grafo{
+	int n;
+	vector < vector< int > > aristas;
+	Grafo( int N ): n( N ), aristas( n, vector<int>() ){}
+
+	void AgregarArista( int u, int v ){
+		aristas[u].push_back( v );
+	}
+};
+
+int main(){
+	return 0;
+}
